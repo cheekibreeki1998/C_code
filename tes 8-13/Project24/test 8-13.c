8950f0375d77c645c56a32942d7f43c51d9dc456
cheekibreeki1998 <541324483@qq.com>
2020-08-13 23:21:13 +0800

    求1到100之间所有是3的倍数的数

---
+++ tes 8-13/Project24/test 8-13.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+		int i = 1;
+		while (i <= 100)
+		{
+			if (i % 3 == 0)
+			{ 
+				printf("%d\n", i);
+			}
+		i++;
+		}
+	
+		return 0;
+	}
+	
+
+
